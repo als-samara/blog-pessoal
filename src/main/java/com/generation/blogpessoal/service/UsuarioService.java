b41727c5a54ec06b4a08ee6083017494e95f72be
als-samara <samaraalmeida379@gmail.com>
2024-01-31 14:31:48 -0300

    set default picture

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -33,6 +33,9 @@ public class UsuarioService {
 		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
 			return Optional.empty();
 
+		if(usuario.getFoto().isBlank())
+			usuario.setFoto("https://i.imgur.com/Zz4rzVR.png");
+		
 		usuario.setSenha(criptografarSenha(usuario.getSenha()));
 
 		return Optional.of(usuarioRepository.save(usuario));
